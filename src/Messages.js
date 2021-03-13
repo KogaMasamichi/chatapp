4ea16fa63dc7c960da728d84f449e7e418c0c8f5
K.Mizuno <mizu.vbct6.h2o@gmail.com>
2021-03-13 13:04:30 +0900

    削除ボタン(idの取得から)

--- src/Messages.js
+++ src/Messages.js
@@ -1,8 +1,19 @@
+import firebase from "./config/firebase";
+
 const Messages = ({message}) => {
+
+  const handleDelete = () => {
+    firebase.firestore().collection("messages").doc(id).delete().then(() => {
+      console.log("Document successfully deleted!");
+  }).catch((error) => {
+      console.error("Error removing document: ", error);
+  });
+  }
+
   return (
 
       <li>
-        {message.user} : {message.content}
+        {message.user} : {message.content} <button onClick={handleDelete}>削除</button>
       </li>
   )
   
